1317ac7464eeb67576c3692445c9d9b65de4c09e
dineshdabhi <dineshdabhi@cloud.upwork.com>
2024-03-03 12:16:16 +0530

    express with mongo and static file

---
+++ express-server-with-mongo-static/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const {Schema} = require("mongoose");
+const mongoose = require("mongoose");
+const app = express()
+const port = 3000
+app.use(express.json());
+
+app.use(express.static('public'))
+
+mongoose.connect('mongodb://localhost:27017/localDb');
+const UserModel = mongoose.model('users', new Schema({userName: String, password: String}));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/api/login', async (req, res) => {
+    console.log('req.body', req.body);
+    const response = await UserModel.findOne({userName: req.body.userName, password: req.body.password});
+    console.log('response ===> ', response);
+
+    if (response) {
+        res.send('log in success')
+    } else {
+        res.status(401);
+        res.send('login failure');
+    }
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
---
+++ simple-express-static-File/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json());
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/api/login', (req, res) => {
+    console.log('req.body', req.body);
+    if(req.body.userName === "admin" && req.body.password === "admin")
+    {
+        res.send('log in success')
+    }
+    else
+    {
+        res.status(401);
+        res.send('login failure');
+    }
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
